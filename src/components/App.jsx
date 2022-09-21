74b038f7be67ade8e74443499af8eae63a35c53a
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-09-21 17:31:05 +0300

    add App hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,116 +1,103 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import MainPage from './MainPage/MainPage';
 import TransactionHistoryPage from './TransactionHistoryPage';
 import Container from './Container';
 
-export class App extends Component {
-  state = {
-    activePage: 'main',
-    expensive: [],
-    income: [],
-    expensiveCategories: [],
-    incomeCategories: [],
-  };
-  componentDidUpdate(_, prevState) {
-    if (prevState.expensive !== this.state.expensive) {
-      localStorage.setItem('expensive', JSON.stringify(this.state.expensive));
-    }
-    if (prevState.income !== this.state.income) {
-      localStorage.setItem('income', JSON.stringify(this.state.income));
-    }
-    if (prevState.expensiveCategories !== this.state.expensiveCategories) {
-      localStorage.setItem(
-        'expensiveCategories',
-        JSON.stringify(this.state.expensiveCategories)
-      );
-    }
-    if (prevState.incomeCategories !== this.state.incomeCategories) {
-      localStorage.setItem(
-        'incomeCategories',
-        JSON.stringify(this.state.incomeCategories)
-      );
-    }
-  }
+const getInitialState = KEY => {
+  return JSON.parse(localStorage.getItem(KEY)) || [];
+};
+const setToLS = (KEY, data) => {
+  return localStorage.setItem(KEY, JSON.stringify(data));
+};
+export const App = () => {
+  const [activePage, setActivePage] = useState('main');
+  const [expensive, setExpensive] = useState(getInitialState('expensive'));
+  const [income, setIncome] = useState(getInitialState('income'));
+  const [expensiveCategories, setExpensiveCategories] = useState(
+    getInitialState('expensiveCategories')
+  );
+  const [incomeCategories, setIncomeCategories] = useState(
+    getInitialState('incomeCategories')
+  );
 
-  componentDidMount() {
-    const income = JSON.parse(localStorage.getItem('income')) || [];
-    const expensive = JSON.parse(localStorage.getItem('expensive')) || [];
-    const incomeCategories =
-      JSON.parse(localStorage.getItem('incomeCategories')) || [];
-    const expensiveCategories =
-      JSON.parse(localStorage.getItem('expensiveCategories')) || [];
-    this.setState({
-      income,
-      expensive,
-      incomeCategories,
-      expensiveCategories,
-    });
-  }
+  useEffect(() => {
+    setToLS('expensive', expensive);
+  }, [expensive]);
+  useEffect(() => {
+    setToLS('income', income);
+  }, [income]);
+  useEffect(() => {
+    setToLS('expensiveCategories', expensiveCategories);
+  }, [expensiveCategories]);
+  useEffect(() => {
+    setToLS('incomeCategories', incomeCategories);
+  }, [incomeCategories]);
 
-  changePageHandler = (page = 'main') => {
-    this.setState({ activePage: page });
+  const changePageHandler = (page = 'main') => {
+    setActivePage(page);
   };
 
-  addTransaction = transaction => {
+  const addTransaction = transaction => {
     const { transactionType } = transaction;
-    this.setState(prevState => ({
-      [transactionType]: [...prevState[transactionType], transaction],
-    }));
+    if (transactionType === 'income') {
+      setIncome(prevIncome => {
+        return [...prevIncome, transaction];
+      });
+    }
+    if (transactionType === 'expensive') {
+      setExpensive(prevExpensive => {
+        return [...prevExpensive, transaction];
+      });
+    }
   };
 
-  addCategory = (category, transactionType) => {
-    this.setState(prev => {
-      return transactionType === 'expensive'
-        ? { expensiveCategories: [...prev.expensiveCategories, category] }
-        : { incomeCategories: [...prev.incomeCategories, category] };
-    });
-  };
-  deleteCategory = (id, transactionType) => {
-    this.setState(prev => {
-      return transactionType === 'expensive'
-        ? {
-            expensiveCategories: prev.expensiveCategories.filter(
-              expCategory => expCategory.id !== id
-            ),
-          }
-        : {
-            incomeCategories: prev.incomeCategories.filter(
-              incCategory => incCategory.id !== id
-            ),
-          };
-    });
+  const addCategory = (category, transactionType) => {
+    if (transactionType === 'income') {
+      setIncomeCategories(prevIncome => {
+        return [...prevIncome, category];
+      });
+    }
+    if (transactionType === 'expensive') {
+      setExpensiveCategories(prevExpensive => {
+        return [...prevExpensive, category];
+      });
+    }
   };
 
-  render() {
-    const {
-      expensive,
-      income,
-      activePage,
-      expensiveCategories,
-      incomeCategories,
-    } = this.state;
-    return (
-      <Container>
-        {this.state.activePage === 'main' ? (
-          <MainPage
-            changePageHandler={this.changePageHandler}
-            addTransaction={this.addTransaction}
-            addCategory={this.addCategory}
-            deleteCategory={this.deleteCategory}
-            categories={{
-              expensiveCategories,
-              incomeCategories,
-            }}
-          />
-        ) : (
-          <TransactionHistoryPage
-            transactionType={activePage}
-            changePageHandler={this.changePageHandler}
-            transactions={activePage === 'expensive' ? expensive : income}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+  const deleteCategory = (id, transactionType) => {
+    if (transactionType === 'expensive') {
+      setExpensiveCategories(prevExpensive => {
+        return prevExpensive.filter(expCategory => expCategory.id !== id);
+      });
+    }
+    if (transactionType === 'income') {
+      setIncomeCategories(prevIncome => {
+        return prevIncome.filter(incCategory => incCategory.id !== id);
+      });
+    }
+  };
+
+  return (
+    <Container>
+      {activePage === 'main' ? (
+        <MainPage
+          changePageHandler={changePageHandler}
+          addTransaction={addTransaction}
+          addCategory={addCategory}
+          deleteCategory={deleteCategory}
+          categories={{
+            expensiveCategories,
+            incomeCategories,
+          }}
+        />
+      ) : (
+        <TransactionHistoryPage
+          transactionType={activePage}
+          changePageHandler={changePageHandler}
+          transactions={activePage === 'expensive' ? expensive : income}
+        />
+      )}
+    </Container>
+  );
+};
