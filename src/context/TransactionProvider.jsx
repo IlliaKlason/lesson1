278ab137aedec8d9f4d8d0d5a0b6db745ae71d71
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-09-22 17:51:53 +0300

    App useRef

--- src/context/TransactionProvider.jsx
+++ src/context/TransactionProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useRef } from 'react';
 
 import { getInitialState, setToLS } from '../helpers';
 
@@ -7,6 +7,11 @@ export const TransactionContext = createContext();
 const TransactionProvider = ({ children }) => {
   const [expensive, setExpensive] = useState(getInitialState('expensive', []));
   const [income, setIncome] = useState(getInitialState('income', []));
+
+  const isFirstRenderRef = useRef({
+    i: true,
+    e: true,
+  });
 
   const addTransaction = transaction => {
     const { transactionType } = transaction;
@@ -23,9 +28,19 @@ const TransactionProvider = ({ children }) => {
   };
 
   useEffect(() => {
+    if (isFirstRenderRef.current.e) {
+      isFirstRenderRef.current.e = false;
+      return;
+    }
     setToLS('expensive', expensive);
   }, [expensive]);
+
   useEffect(() => {
+    // isFirstRenderRef.current = true;
+    if (isFirstRenderRef.current.i) {
+      isFirstRenderRef.current.i = false;
+      return;
+    }
     setToLS('income', income);
   }, [income]);
   return (
